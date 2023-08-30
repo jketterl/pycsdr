1869b601b34e0c5a2f90571a62477396062800ff
Jakob Ketterl <jakob.ketterl@gmx.de>
2023-08-30 17:30:44 +0200

    we can do a tcpsource for unsigned char now

--- src/tcpsource.cpp
+++ src/tcpsource.cpp
@@ -24,6 +24,8 @@ static int TcpSource_init(TcpSource* self, PyObject* args, PyObject* kwds) {
             self->source = new Csdr::TcpSource<Csdr::complex<short>>(inet_addr("127.0.0.1"), port);
         } else if (self->outputFormat == FORMAT_COMPLEX_FLOAT) {
             self->source = new Csdr::TcpSource<Csdr::complex<float>>(inet_addr("127.0.0.1"), port);
+        } else if (self->outputFormat == FORMAT_COMPLEX_CHAR) {
+            self->source = new Csdr::TcpSource<Csdr::complex<unsigned char>>(inet_addr("127.0.0.1"), port);
         } else {
             PyErr_SetString(PyExc_ValueError, "unsupported tcpsource format");
             return -1;
